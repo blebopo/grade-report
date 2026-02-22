b2818c1b3333abaf4be786018e8dd0057a2b09c1
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-02-23 01:28:36 +0200

    Student data added.

---
+++ src/data/StudentData.tsx
@@ -0,0 +1,12 @@
+export const students = [
+  { name: "Ayanda Ndlovu", score: 97 },
+  { name: "Boitumelo Thulo", score: 85 },
+  { name: "Mohit Gupta", score: 73 },
+  { name: "Tevin Reddy", score: 91 },
+  { name: "Jake Swanepoel", score: 61 },
+  { name: "Amanda DuPont", score: 55 },
+  { name: "Thabo Mbheki", score: 88 },
+  { name: "Jacob Zuma", score: 44 },
+  { name: "Kevin Durant", score: 79 },
+  { name: "Elon Musk", score: 33 },
+];
