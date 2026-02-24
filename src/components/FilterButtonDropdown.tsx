c205bdc739fea8c225fc3c9b2e3e80d1c52b50ef
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-02-24 04:05:31 +0200

    Feature: Dropdown button for filters added.

---
+++ src/components/FilterButtonDropdown.tsx
@@ -0,0 +1,9 @@
+export default function FilterButtonDropdownComponent({}) {
+   return (
+    <>
+      <select name="filters" id="filters" multiple>
+        <option value="filter-button">button</option>
+      </select>
+    </>
+   ); 
+}
--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -2,22 +2,39 @@ import { gradeRanges } from "../types/grades";
 import "../styles/Legend.css";
 import AddFilterButtonComponent from "./AddFilterButton";
 import FilterButtonComponent from "./FilterButton";
+import FilterButtonDropdownComponent from "./FilterButtonDropdown";
 
 export default function LegendComponent() {
   // Determine buttons to display
-  const displayedFilters = [...gradeRanges];
+  const displayedFilters = [
+    ...gradeRanges.map((grade) => {
+      return { 
+        grade, 
+        isSelected: false 
+      };
+    }),
+  ];
   const hiddenFilters = [...gradeRanges];
   // If buttons have been selected add them to buttons to display
   function removeFilter() {}
-  function addFilter() {}
+  function addFilter() {
+    // On click show dropdown
+    displayedFilters.filter(grade => !grade.isSelected)
+  }
   return (
     <div className="grade-legend">
-      {displayedFilters.map((grade) => {
-        return (
-          <FilterButtonComponent grade={grade} removeFilter={removeFilter} />
-        );
-      })}
+      {displayedFilters
+        .filter((grade) => grade.isSelected)
+        .map((grade) => {
+          return (
+            <FilterButtonComponent
+              grade={grade.grade}
+              removeFilter={removeFilter}
+            />
+          );
+        })}
       <AddFilterButtonComponent addFilter={addFilter} />
+      <FilterButtonDropdownComponent />
     </div>
   );
 }
